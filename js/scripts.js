d4337cbca8f6afb7ad045ada2b7a39c17b9f5c6d
zAndrezz <84895875+zAndrezz@users.noreply.github.com>
2021-06-15 22:38:37 -0500

    created pokemonList array
    created pokemonList array with pokemon objects

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+let pokemonList = [{
+        name: 'Raichu',
+        height: 0.8,
+        types: ['electric'],
+        generation: 1,
+    },
+    {
+        name: 'Keldeo',
+        height: 1.4,
+        types: ['fighting', 'water'],
+        generation: 5,
+    },
+    {
+        name: 'Ivysaur',
+        height: 1,
+        types: ['grass', 'poison'],
+        generation: 1,
+    },
+];
