cb3f274e2b555211a389cd57e094ff7d1daba5cb
zAndrezz <84895875+zAndrezz@users.noreply.github.com>
2021-07-02 01:49:00 -0500

    added style for title and crumbled list and fixed random img error
    added style for crumbled pokemon list.
    
    added style to pokedex title
    
    fixed random img error

--- js/scripts.js
+++ js/scripts.js
@@ -97,7 +97,7 @@ let pokemonRepository = (function() {
 
         // setting `src` property to set the actual element's `src` attribute
         // this also works on <img> elements selected by querySelector() method, it is not specific for <img> elements created with createElement() methods
-        myImage.src = 'https://picsum.photos/300/300';
+
 
         container.appendChild(myImage);
 
